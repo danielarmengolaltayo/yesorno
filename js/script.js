a192efdf2293030045ee36c227afce6d6cf4489a
Daniel Armengol Altayó <daniel@armengol-altayo.com>
2022-05-31 20:15:22 +0200

    test

--- js/script.js
+++ js/script.js
@@ -1,25 +1,31 @@
-let yes, no;
+// let yes, no;
+const yes = new Howl({
+  src: ['audio/yes.mp3']
+});
+const no = new Howl({
+  src: ['audio/no.mp3']
+});
 const button = document.getElementById("button");
 
 button.addEventListener('click', function () {
   if (button.textContent === "LOAD") {
-    yes = new Audio('audio/yes.mp3');
-    no = new Audio('audio/no.mp3');
+    // yes = new Audio('audio/yes.mp3');
+    // no = new Audio('audio/no.mp3');
     button.textContent = "START"
-    button.style.background = "pink";
+    button.style.background = "orange";
   } else if (button.textContent === "START") {
     console.log("START");
     timer = setInterval(function () {
       let i = Math.floor(Math.random() * 2);
       if (i === 0) {
         //amb mobile sembla que no em deixa reproduir dos audios alhora, el que m'obliga a parar i reiniciar el currentTime
-        yes.pause();
-        no.currentTime = 0; 
+        // yes.pause();
+        // no.currentTime = 0; 
         no.play();
         console.log("NO");
       } else if (i === 1) {
-        no.pause();
-        yes.currentTime = 0;
+        // no.pause();
+        // yes.currentTime = 0;
         yes.play();
         console.log("YES");
       } else {
