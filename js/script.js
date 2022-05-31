2460c4e94f536bea3455c30840c46154611c990c
Daniel Armengol Altayó <daniel@armengol-altayo.com>
2022-05-31 17:07:40 +0200

    test

--- js/script.js
+++ js/script.js
@@ -6,20 +6,20 @@ button.addEventListener('click', function () {
     yes = new Audio('audio/yes.mp3');
     no = new Audio('audio/no.mp3');
     button.textContent = "START"
-    button.style.background = "limegreen";
+    button.style.background = "pink";
   } else if (button.textContent === "START") {
     console.log("START");
     timer = setInterval(function () {
       let i = Math.floor(Math.random() * 2);
       if (i === 0) {
         //amb mobile sembla que no em deixa reproduir dos audios alhora, el que m'obliga a parar i reiniciar el currentTime
-        // yes.pause();
-        // no.currentTime = 0; 
+        yes.pause();
+        no.currentTime = 0; 
         no.play();
         console.log("NO");
       } else if (i === 1) {
-        // no.pause();
-        // yes.currentTime = 0;
+        no.pause();
+        yes.currentTime = 0;
         yes.play();
         console.log("YES");
       } else {
