80a9e270412580e5c2e0568e501d06c889ebfbb5
Daniel Armengol Altayó <daniel@armengol-altayo.com>
2022-05-31 16:06:43 +0200

    trying to fix audio on mobile

--- js/script.js
+++ js/script.js
@@ -1,17 +1,23 @@
+let yes;
+let no;
 const button = document.getElementById("button");
 
 button.addEventListener('click', function () {
-  const yes = new Audio('audio/yes.mp3');
-  const no = new Audio('audio/no.mp3');
   if (button.textContent === "START") {
     console.log("START");
     timer = setInterval(function () {
       let i = Math.floor(Math.random() * 2);
       if (i === 0) {
+        if(!no){
+          no = new Audio('audio/no.mp3');
+        }
         no.muted = false;
         no.play();
         console.log("NO");
       } else if (i === 1) {
+        if(!yes){
+          yes = new Audio('audio/yes.mp3');
+        }
         yes.muted = false;
         yes.play();
         console.log("YES");
