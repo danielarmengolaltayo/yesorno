95ea96b3988c8e9cc0e8ffcf7dc05300b674cfe3
Daniel Armengol Altayó <daniel@armengol-altayo.com>
2022-05-31 16:53:39 +0200

    test

--- js/script.js
+++ js/script.js
@@ -6,19 +6,19 @@ button.addEventListener('click', function () {
     yes = new Audio('audio/yes.mp3');
     no = new Audio('audio/no.mp3');
     button.textContent = "START"
-    button.style.background = "pink";
+    button.style.background = "blue";
   } else if (button.textContent === "START") {
     console.log("START");
     timer = setInterval(function () {
       let i = Math.floor(Math.random() * 2);
       if (i === 0) {
         yes.pause();
-        yes.currentTime = 0;
+        no.currentTime = 0;
         no.play();
         console.log("NO");
       } else if (i === 1) {
         no.pause();
-        no.currentTime = 0;
+        yes.currentTime = 0;
         yes.play();
         console.log("YES");
       } else {
